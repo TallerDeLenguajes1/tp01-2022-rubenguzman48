7a9527b50d2bf800e88df9abc9eb1d209b3a0bc4
Ruben Guzman <rubenguzman48@gmail.com>
2022-04-09 19:32:05 -0300

    commit de hoy

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,6 +2,23 @@
 
 int main()
 {
-    printf("Hola Mundo");
+    int Dato = 4; 
+    int *PtrDato;
+
+    PtrDato = &Dato;
+
+    printf("Hola Mundo\n");
+    printf("Contenido del puntero: %d\n", *PtrDato); //Con este linea mostramos el contenido de la variable a la que apunta el puntero
+    printf("Direccion de memoria almacenada por el puntero: %d\n", PtrDato); //Con esta linea mostramos la direccion de memoria a la que apunta el puntero
+    printf("Direccion de memoria de la varibale: %d\n", &Dato); //Con esta linea mostramos la direccion de memoria de la variable
+    printf("Direccion de memoria de puntero: %d\n", &PtrDato);
+    printf("Tamaño de memoria utilizado por la variable es de %zd bytes\n", sizeof(Dato));
+
+
 return 0;    
 }
+
+
+
+
+
